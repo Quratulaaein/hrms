ff36f98f037cc085545fb96d3e8fc545cad076c9
Qurat ul Aaein <122012750+Quratulaaein@users.noreply.github.com>
2026-02-19 15:48:50 +0530

    Fix groq config for railway

--- app/config.py
+++ app/config.py
@@ -1,19 +1,24 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    # GHL
     GHL_API_KEY: str
     GHL_LOCATION_ID: str
     GHL_API_URL: str
-
     GHL_SCORE_FIELD_ID: str
     GHL_LINK_FIELD_ID: str
     GHL_USERNAME_FIELD_ID: str
     GHL_PASSWORD_FIELD_ID: str
 
+    # GROQ
+    GROQ_API_KEY: str
+    GROQ_BASE_URL: str = "https://api.groq.com/openai/v1"
+
     class Config:
         env_file = ".env"
-        extra = "allow"
+
 
 settings = Settings()
