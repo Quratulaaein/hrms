d45e9099171931fc023c96828cf92daeb183f2df
Qurat ul Aaein <122012750+Quratulaaein@users.noreply.github.com>
2026-02-20 14:25:52 +0530

    db updated

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = "postgresql://postgres:%40biVMj2z%40t6Wszy@db.eiejdgqjxpvshrpfjjju.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres.eiejdgqjxpvshrpfjjju:%40biVMj2z%40t6Wszy@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
